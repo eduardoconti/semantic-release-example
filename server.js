04ace169150c1e6b918a5d5590676aa8d441dc0c
Eduardo Conti <es.eduardoconti@gmail.com>
2024-01-06 15:20:19 -0300

    feat: add health check endpoint

--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 const http = require('http')
 
-const server = http.createServer();
+const routes = [{
+  method: 'GET',
+  path: '/health-check',
+  controller: () => {
+    return {
+      status: 200,
+      description: 'app is running'
+    }
+  }
+}]
+
+const server = http.createServer(
+  async (req, res) => {
+    const path = parseUrl(req).pathname
+    const method = req.method
+    const route = routes.find((e) => e.method === method && path === e.path)
+
+    if (route) {
+      const result = route.controller()
+
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+
+      res.write(
+        JSON.stringify(result),
+      );
+
+    } else {
+      res.writeHead(404, {
+        "Content-Type": "application/problem+json",
+      });
+
+      res.write(
+        JSON.stringify({
+          status: 404,
+          title: 'Not found',
+          description: 'Route not found',
+          type: 'about:blank'
+        }),
+      );   
+    }
+   
+    res.end()
+  },
+);
+
+const parseUrl = (request) => {
+  return new URL(request.url, `http://${request.headers.host}`);
+};
 
 server.listen(3000, () => {
   console.log(`server started on port: 3000`);
